fab329e2dc564a87b802552a900499075b055238
Ninjacornix <ninjacornix@Ninjacornixs-MacBook-Pro.local>
2021-12-07 12:46:56 +0100

    added day 7

---
+++ 7/part1.cc
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <algorithm>
+#include <sstream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    string line;
+    ifstream file;
+    file.open("input.txt");
+    getline(file, line);
+    stringstream ss(line);
+    vector<int> v;
+    for (int i = 0; ss >> i;)
+    {
+        v.push_back(i);
+        if (ss.peek() == ',')
+        {
+            ss.ignore();
+        }
+    }
+
+    int max = v[0];
+    for (int i = 0; i < v.size(); i++)
+    {
+        if (v[i] > max)
+        {
+            max = v[i];
+        }
+    }
+
+    int arr[max];
+    int sum = 0;
+    vector<int> fuel;
+    for (int i = 0; i < max; i++)
+    {
+        sum = 0;
+        for (int j = 0; j < v.size(); j++)
+        {
+            sum += abs(v[j] - i);
+        }
+        fuel.push_back(sum);
+    }
+
+    int min = fuel[0];
+    for (int i = 0; i < fuel.size(); i++)
+    {
+        if (fuel[i] < min)
+        {
+            min = fuel[i];
+        }
+    }
+
+    cout << min << endl;
+
+    return 0;
+}
